145315d04184c0a31f94f4f76869b2b34145e771
aleks208 <amadej72@gmail.com>
2023-07-24 18:03:16 +0100

    adding protocol and port columns

--- Analyzer-Code.py
+++ Analyzer-Code.py
@@ -58,16 +58,20 @@ def find_ips(packet):
         # Get MAC addresses
         src_mac = packet['Ether'].src
         dst_mac = packet['Ether'].dst
+        # Get Protocol and Port
+        protocol = packet['IP'].proto
+        src_port = packet['IP'].sport
+        dst_port = packet['IP'].dport
         # If the IP matches the entered sub domain display it along with it's MAC address
         if src_ip[0:len(subdomain)] == subdomain:
             if src_ip not in src_ip_dict:
                 src_ip_dict[src_ip] = (src_mac, [dst_ip])
                 vendor = get_mac_vendors(src_mac)
                 src_mac = src_mac + " which is " + vendor
-                row = treeV.insert('', index=tk.END, text=src_ip, values=(src_mac, vendor))
+                row = treeV.insert('', index=tk.END, text=src_ip, values=(src_mac, protocol, src_port))
                 vendor = get_mac_vendors(dst_mac)
                 dst_mac = dst_mac + " which is " + vendor
-                treeV.insert(row, tk.END, text=dst_ip, values=(dst_mac,))
+                treeV.insert(row, tk.END, text=dst_ip, values=(dst_mac, protocol, dst_port))
                 treeV.pack(fill=tk.X)
             else:
                 if dst_ip not in src_ip_dict[src_ip][1]:
@@ -76,7 +80,7 @@ def find_ips(packet):
                     src_ip_dict[src_ip][1].append(dst_ip)
                     cur_item = treeV.focus()
                     if treeV.item(cur_item)['text'] == src_ip:
-                        treeV.insert(cur_item, tk.END, text=dst_ip, values=(dst_mac,))
+                        treeV.insert(cur_item, tk.END, text=dst_ip, values=(dst_mac, protocol, dst_port))
 
 
 # Setting key variables
@@ -88,7 +92,7 @@ src_ip_dict = collections.defaultdict(list)
 #------------------ GUI ------------------#
 # Creating main window
 root = tk.Tk() 
-root.geometry('600x600')
+root.geometry('650x600')
 root.title('Network Analyzer')
 root.configure(background='#F5F5F5')
 
@@ -109,13 +113,20 @@ tk.Button(button_frame, text='Stop', command=stop_button, width=6, font="Inconso
 button_frame.pack(pady=10)
 
 # Creating the tree view widget
-treeV = ttk.Treeview(root, height=350, columns=("mac_address",))
-treeV.column("#0", width=50, minwidth=50)
-treeV.column("mac_address", width=150, minwidth=150)
+treeV = ttk.Treeview(root, height=350, columns=("mac_address", "protocol", "port"))
+treeV.column("#0", width=30, minwidth=30)
+treeV.column("mac_address", width=170, minwidth=170)
+treeV.column("protocol", width=15, minwidth=15)
+treeV.column("port", width=15, minwidth=15)
 treeV.heading("#0", text="IP Address")
 treeV.heading("mac_address", text="MAC Address")
+treeV.heading("protocol", text="Protocol")
+treeV.heading("port", text="Port")
 style = ttk.Style(root)
 style.theme_use("clam")
 ttk.Style().configure("Treeview", background="#98FF98", foreground="black", fieldbackground="#98FF98", font="Inconsolata 10")
 
 root.mainloop()
+
+#seperate protocol and port
+#figure out why vendor is showing up in right column
